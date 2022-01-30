102742f919e2f88e0cc9133ca6ac4ebf98168169
Deepak Rawat <98208521+deepakrawat1@users.noreply.github.com>
2022-01-30 17:05:38 +0530

    Add files via upload

---
+++ 1. Print Message.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Hello Line");
+    Console.Write("Hello Without Line");
+    Console.Write("Hello Without Line");
+    Console.Write("Hello Without Line");
+  }
+}
